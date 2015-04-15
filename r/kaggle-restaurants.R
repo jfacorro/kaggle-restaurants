447d14e5dd2e219276c0743a1858b2027081ed88
Juan Facorro <juan.facorro@gmail.com>
2015-04-15 11:23:46 -0300

    Added R script.

---
+++ r/kaggle-restaurants.R
@@ -0,0 +1,42 @@
+library("e1071")
+library("rpart")
+library("cvTools")
+
+generate_output <- function(model, test_set, filename) {
+  predictions <- predict(model, test_set)
+  indexes <- 0:(length(predictions) - 1)
+  predictionsStr <- paste(indexes, formatC(predictions, format = "f", digits = 1), sep = ",")
+  file_conn <- file(filename)
+  writeLines(c("Id,Prediction", predictionsStr), file_conn)
+  close(file_conn)
+}
+
+process_dataset <- function(dataset) {
+  dataset$Open.Date <- Sys.Date() - as.Date(dataset$Open.Date, format = "%m/%d/%Y") 
+  dataset$Type[Type == "MB"] <- "IL"
+  datatest$Type <- droplevels(datatest$Type)
+}
+
+load_data <- function() {
+  train <- read.csv("~/dev/kaggle-restaurants/resources/train.csv")
+  test <- read.csv("~/dev/kaggle-restaurants/resources/test.csv")
+}
+
+main <- function() {
+  ignore_fields <- c(-1, -3)
+  
+  svm.model <- svm(revenue ~ ., data = train[, ignore_fields], cost = 100, gamma = 0.0001)
+  cvFit(svm.model, data = train, y = train$revenue, K = 5, R = 10)
+  # svm.preds <- predict(svm.model, newdata = test[, ignore_fields])
+  
+  rpart.model <- rpart(revenue ~ ., data = train[, ignore_fields], control = rpart.control(cp = 0.01, minsplit = 2))
+  cvFit(rpart.model, data = train, y = train$revenue, K = 5, R = 10)
+  # rpart.preds <- predict(rpart.model, test[, ignore_fields])
+  
+  # generate_output(svm.model, test[, ignore_fields], "svm.output")
+  # generate_output(rpart.model, test[, ignore_fields], "rpart.output")
+  str(train)
+  str(test)
+  length(unique(train$City))
+  length(unique(test$City))
+}
